85714898cef1ce9558d738ffb8132dc2837d42d8
Vorleak Yek <vorleakyek@gmail.com>
2024-02-18 12:41:39 -0800

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 const config = {
+  //WebGL (Web graphics libray) JS API for rendering 2D and 3D graphics 
   type: Phaser.AUTO,
-  width: 800,
+  width:800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    //Arcade physics plugin, manages physics simulation
+    default: 'arcade'
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create
+    //update: update
   }
-};
+}
+
+// Loading assetts, such as images, music, animations ...
+function preload() {
+  debugger
+}
+
+//
+function create() {
+  debugger
+}
 
 new Phaser.Game(config);
 
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
